129fe986563846aadb298f4fe9f7a833cce34d67
veena-kptl <veena.p@krishtechnolabs.com>
2021-03-31 12:39:56 +0530

    examples

---
+++ NewCurd/Module/Model/Post.php
@@ -0,0 +1,27 @@
+<?php
+namespace NewCurd\Module\Model;
+class Post extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
+{
+	const CACHE_TAG = 'helloworld_post';
+
+	protected $_cacheTag = 'helloworld_post';
+
+	protected $_eventPrefix = 'helloworld_post';
+
+	protected function _construct()
+	{
+		$this->_init('NewCurd\Module\Model\ResourceModel\Post');
+	}
+
+	public function getIdentities()
+	{
+		return [self::CACHE_TAG . '_' . $this->getId()];
+	}
+
+	public function getDefaultValues()
+	{
+		$values = [];
+
+		return $values;
+	}
+}
