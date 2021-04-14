6e266adfd1562847621d4d2b45352be2429114e9
veena-kptl <veena.p@krishtechnolabs.com>
2021-04-14 20:34:40 +0530

    ViewModels and cache type

---
+++ CacheType/CustomModule/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CacheType_CustomModule',
+    __DIR__
+);
---
+++ PassArgument/CustomModule/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PassArgument_CustomModule',
+    __DIR__
+);
